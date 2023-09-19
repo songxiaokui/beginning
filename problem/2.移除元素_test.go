fd36e50b90e8e7a6cb8d6edb953d8df796d37e14
austsxk <austsxk@163.com>
2023-09-19 14:17:04 +0800

    D2 leetcode27 原地移除数组中指定元素

---
+++ problem/2.移除元素_test.go
@@ -0,0 +1,12 @@
+package problem
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_removeElement(t *testing.T) {
+	l1 := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	target := 2
+	fmt.Println(removeElement(l1, target))
+}
