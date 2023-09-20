c05c782f3dc401a7471eb9401d665c913fce1945
austsxk <austsxk@163.com>
2023-09-20 09:10:44 +0800

    D3 4.80增加测试案例

---
+++ problem/4.80. 删除有序数组中的重复项 II_test.go
@@ -0,0 +1,13 @@
+package problem
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_removeDuplicatesII(t *testing.T) {
+	l3 := []int{1, 1, 1, 2, 2, 3}
+	// l3 := []int{1, 1, 1}
+	fmt.Println(removeDuplicatesII2(l3))
+	fmt.Println(l3)
+}
